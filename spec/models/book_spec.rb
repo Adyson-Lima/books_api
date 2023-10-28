d1a7118cba0acb461d673dd7100fe6eb187a2893
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-10-28 09:56:47 -0300

    3° commit - tdd model: title consegue ser preenchido? GREEN

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Book, type: :model do
   describe 'Teste de preenchimento dos campos do model Book' do
 
     it 'title consegue ser preenchido?' do
-      @book.title = ""
+      @book.title = "Ruby 3"
       expect(@book.title).to eq("Ruby 3")
     end
 
