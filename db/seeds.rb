b438d3e4cb64dcdd971412a4021b41d630dd1b20
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-10-28 11:35:17 -0300

    20° commit - seeds.rb configurado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts 'Gerando books...'
+
+10.times do |i|
+  Book.create(
+    title: ["ruby", "rspec", "rails"].sample, 
+    description: Faker::Book.publisher   
+    )
+end
+
+puts 'books gerados com sucesso!'
