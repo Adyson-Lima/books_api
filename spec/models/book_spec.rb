6b3ba496da13087a1a893a010e40f317e308ffd3
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-10-28 09:52:58 -0300

    2° commit - tdd model: title consegue ser preenchido? RED

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+
+  before { @book = Book.new }
+
+  describe 'Teste de preenchimento dos campos do model Book' do
+
+    it 'title consegue ser preenchido?' do
+      @book.title = ""
+      expect(@book.title).to eq("Ruby 3")
+    end
+
+  end
+      
+end
